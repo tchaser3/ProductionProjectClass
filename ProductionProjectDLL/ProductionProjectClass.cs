b6617b34b1c82028ec9090650ebe04a3b7b4f8f1
Terry Holmes <tchaser3@outlook.com>
2020-01-04 13:16:02 -0500

    Add project files.

---
+++ ProductionProjectDLL/ProductionProjectClass.cs
@@ -0,0 +1,110 @@
+﻿/* Title:       Production Project Classs
+ * Date:        1-4-20
+ * Author:      Terry Holmes
+ * 
+ * Description: This is used for production projects */
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NewEventLogDLL;
+
+namespace ProductionProjectDLL
+{
+    public class ProductionProjectClass
+    {
+        //setting up the classes
+        EventLogClass TheEventLogClass = new EventLogClass();
+
+        ProductionProjectsDataSet aProductionProjectsDataSet;
+        ProductionProjectsDataSetTableAdapters.productionprojectsTableAdapter aProductionProjectsTableAdapter;
+
+        InsertProductionProjectEntryTableAdapters.QueriesTableAdapter aInsertProductionProjectTableAdapter;
+
+        UpdateProductionProjectStatusEntryTableAdapters.QueriesTableAdapter aUpdateProductionProjectStatusTableAdapter;
+
+        FindProdutionProjectsByAssignedProjectIDDataSet aFindProductionProjectsByAssignedProjectIDDataSet;
+        FindProdutionProjectsByAssignedProjectIDDataSetTableAdapters.FindProductionProjectByAssignedProjectIDTableAdapter aFindProductionProjectsByAssignedProjectsIDTableAdapter;
+
+        public FindProdutionProjectsByAssignedProjectIDDataSet FindProductionProjectsByAssignedProjectID(string strAssignedProjectID)
+        {
+            try
+            {
+                aFindProductionProjectsByAssignedProjectIDDataSet = new FindProdutionProjectsByAssignedProjectIDDataSet();
+                aFindProductionProjectsByAssignedProjectsIDTableAdapter = new FindProdutionProjectsByAssignedProjectIDDataSetTableAdapters.FindProductionProjectByAssignedProjectIDTableAdapter();
+                aFindProductionProjectsByAssignedProjectsIDTableAdapter.Fill(aFindProductionProjectsByAssignedProjectIDDataSet.FindProductionProjectByAssignedProjectID, strAssignedProjectID);
+            }
+            catch (Exception Ex)
+            {
+                TheEventLogClass.InsertEventLogEntry(DateTime.Now, "Production Project Class // Find Production Projects By Assigned Project ID " + Ex.Message);
+            }
+
+            return aFindProductionProjectsByAssignedProjectIDDataSet;
+        }
+        public bool UpdateProductionProjectStatus(int intTransactionID, int intCurrentStatusID)
+        {
+            bool blnFatalError = false;
+
+            try
+            {
+                aUpdateProductionProjectStatusTableAdapter = new UpdateProductionProjectStatusEntryTableAdapters.QueriesTableAdapter();
+                aUpdateProductionProjectStatusTableAdapter.UpdateProductionProjectStatus(intTransactionID, intCurrentStatusID);
+            }
+            catch (Exception Ex)
+            {
+                TheEventLogClass.InsertEventLogEntry(DateTime.Now, "Production Project Class // Update Production Project Status " + Ex.Message);
+
+                blnFatalError = true;
+            }
+
+            return blnFatalError;
+        }
+        public bool InsertProdutionProject(int intProjectID, int intBusinessLineID, string strBusinessAddress, string strCity, string strBusinessState, int intProjectManagerID, int intAssignedOfficeID,  DateTime datECDDate, int intCurrentStatusID, string strProjectNotes)
+        {
+            bool blnFatalError = false;
+
+            try
+            {
+                aInsertProductionProjectTableAdapter = new InsertProductionProjectEntryTableAdapters.QueriesTableAdapter();
+                aInsertProductionProjectTableAdapter.InsertProductionProject(intProjectID, intBusinessLineID, strBusinessAddress, strCity, strBusinessState, intProjectManagerID, intAssignedOfficeID, DateTime.Now, datECDDate, intCurrentStatusID, strProjectNotes);
+            }
+            catch (Exception Ex)
+            {
+                TheEventLogClass.InsertEventLogEntry(DateTime.Now, "Production Project Class // Insert Production Project " + Ex.Message);
+
+                blnFatalError = true;
+            }
+
+            return blnFatalError;
+        }
+        public ProductionProjectsDataSet GetProductionProjectsInfo()
+        {
+            try
+            {
+                aProductionProjectsDataSet = new ProductionProjectsDataSet();
+                aProductionProjectsTableAdapter = new ProductionProjectsDataSetTableAdapters.productionprojectsTableAdapter();
+                aProductionProjectsTableAdapter.Fill(aProductionProjectsDataSet.productionprojects);
+            }
+            catch (Exception Ex)
+            {
+                TheEventLogClass.InsertEventLogEntry(DateTime.Now, "Production Projects Class // Get Productions Projects Info " + Ex.Message);
+            }
+
+            return aProductionProjectsDataSet;
+        }
+        public void UpdateProductionProjectsDB(ProductionProjectsDataSet aProductionProjectsDataSet)
+        {
+            try
+            {
+                aProductionProjectsTableAdapter = new ProductionProjectsDataSetTableAdapters.productionprojectsTableAdapter();
+                aProductionProjectsTableAdapter.Update(aProductionProjectsDataSet.productionprojects);
+            }
+            catch (Exception Ex)
+            {
+                TheEventLogClass.InsertEventLogEntry(DateTime.Now, "Production Projects Class // Update Productions Projects DB " + Ex.Message);
+            }
+        }
+    }
+}
